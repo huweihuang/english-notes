064ba1b981471b7f5d74db82c8e1e688c3647cb1
huweihuang <huweihuang@foxmail.com>
2023-08-20 15:47:05 +0800

    english notes

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# run in MASTER_DIR
+set -x
+set -e
+
+MESSAGE=$1
+
+rm -fr gh-pages
+git clone -b gh-pages https://github.com/huweihuang/english-notes.git gh-pages
+MASTER_DIR="$(pwd)"
+GHPAGE_DIR="${MASTER_DIR}/gh-pages"
+
+# build
+gitbook build
+
+# clean GHPAGE_DIR
+rm -fr ${GHPAGE_DIR}/*
+
+# copy _book to GHPAGE_DIR
+cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
+
+# git commit
+cd ${GHPAGE_DIR}
+git add --all
+git config user.name huweihuang
+git config user.email huweihuang@foxmail.com
+git commit -m "${MESSAGE}"
+git push origin gh-pages
